50679e4442d04babfa0a37f5840f377caa208fbf
Mike <msanchezmbm@gmail.com>
2022-08-20 15:59:22 -0400

    Added log

--- backend/index.js
+++ backend/index.js
@@ -170,6 +170,7 @@ app.post('/my-sessions', (req, res) => {
             return console.error('Error finding sessions');
         }
         if (result.rows.length > 0) {
+            console.table(result.rows[0]);
             res.send({rows:result.rows});
             return console.log('Sent Sessions');
         }
