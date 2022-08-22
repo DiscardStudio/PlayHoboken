aecfb9949d75589db78791b24c6f60f5a1276589
Mike <msanchezmbm@gmail.com>
2022-08-22 17:16:12 -0400

    Added changes to backend

--- backend/index.js
+++ backend/index.js
@@ -202,9 +202,9 @@ app.get('/find-session', async (req, res) => {
     const date = new Date();
     var today = date.getMonth()+"/"+date.getDay()+"/"+date.getFullYear();
     const result = await callQuery(`
-                select email, first_name, last_name, session_time. game
+                select email, first_name, last_name, session_time game
                 from sessions
-                where sessions.session_data='${today}' and sessions.active='true'
+                where sessions.session_date='${today}' and sessions.active='true'
                 order by session_time
                 `)
     if (result.stack) {
